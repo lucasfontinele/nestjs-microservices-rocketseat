a2849bf197bf627c7049300d8285f26b39ad5b26
Lucas Fontinele <contato@fontinele.dev>
2026-09-07 13:05:46 -0300

    feat: implements fallback

--- src/proxy/service/proxy.service.ts
+++ src/proxy/service/proxy.service.ts
@@ -3,6 +3,8 @@ import { Injectable, Logger } from '@nestjs/common';
 import { serviceConfig } from '../../config/gateway.config.js';
 import { firstValueFrom } from 'rxjs';
 import { CircuitBreakerService } from '../../common/circuit-breaker/circuit-breaker.service.js';
+import { CacheFallbackService } from '../../common/fallback/cache.fallback.js';
+import { DefaultFallbackService } from '../../common/fallback/default.fallback.js';
 
 interface UserInfo {
   userId: string;
@@ -18,7 +20,9 @@ export class ProxyService {
 
   constructor(
     private readonly httpService: HttpService,
-    private readonly circuitBreakerService: CircuitBreakerService
+    private readonly circuitBreakerService: CircuitBreakerService,
+    private readonly cacheFallbackService: CacheFallbackService,
+    private readonly defaultFallbackService: DefaultFallbackService
   ) {}
 
   async proxyRequest(
@@ -33,6 +37,8 @@ export class ProxyService {
     const url = `${service.url}${path}`;
 
     this.logger.log(`Proxying request to ${url} with method ${method}`);
+
+    const fallback = this.createServiceFallback(serviceName, method, path);
 
     return this.circuitBreakerService.executeWithCircuitBreaker(
       async () => {
@@ -53,16 +59,17 @@ export class ProxyService {
           })
         );
 
+        if (method.toUpperCase() === "GET") {
+          this.cacheFallbackService.setCachedData(`${serviceName}:${method}:${path}`, response.data);
+        }
+
         this.logger.log(`Successfully proxied request to ${url}`);
 
         return response.data;
       },
       `${serviceName}:${method}:${path}`,
       { failureThreshold: 3, timeout: 30000, resetTimeout: 30000 },
-      () => {
-        this.logger.error(`Fallback: Service ${serviceName} is currently unavailable. Returning fallback response.`);
-        throw new Error(`Service ${serviceName} is currently unavailable. Please try again later.`);
-      }
+      fallback,
     );
   }
 
@@ -78,6 +85,41 @@ export class ProxyService {
       return { status: 'healthy', data: response.data };
     } catch (error: Error | any) {
       return { status: 'unhealthy', error: error?.message };
+    }
+  }
+
+  private createServiceFallback(serviceName: keyof typeof serviceConfig, method: HttpMethod, path: string) {
+    switch (serviceName) {
+      case "users":
+        if (path.includes('/auth/login')) {
+          return this.defaultFallbackService.createErrorFallback(
+            'users',
+            'Authentication service unavailable',
+          );
+        }
+
+        return this.defaultFallbackService.createErrorFallback(
+          'users',
+          'User service unavailable',
+        );
+      case "products":
+        if (method.toUpperCase() === "GET") {
+          return this.cacheFallbackService.createCacheFallback(
+            `products:${path}`,
+            { products: [], total: 0, page: 1, limit: 10 }
+          );
+        }
+      case "payments":
+      case "checkout":
+        return this.defaultFallbackService.createErrorFallback(
+          serviceName,
+          'Service unavailable',
+        );
+      default:
+        return this.defaultFallbackService.createErrorFallback(
+          serviceName,
+          'Service unavailable',
+        )
     }
   }
 }
